9b4f28ee59b5c1d02199ea2463be6e4de973590a
José Valderrama <jvalderrama2602@gmail.com>
2016-10-13 13:15:02 -0400

    Json con fuentes externas

--- Javascript-Practicas/app.js
+++ Javascript-Practicas/app.js
@@ -1,12 +1,13 @@
-$.getJSON("productos.json",function(producto){
 
+//$.getJSON("productos.json",function(producto){
 
-var totalProductos=producto.length;
+$.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Caracas%2C%20VE%22)&format=json&diagnostics=true&callback=",function(producto){
 
-for (var i=0;i<totalProductos;i++){
+console.log(producto);
 
-console.log(producto[i].nombre);
-
-}
+var frase=producto.query.results.channel.description;
+var humedad=producto.query.results.channel.atmosphere.humidity;
+document.write(frase+"<br>")
+document.write("Humedad: "+humedad )
 
 })
