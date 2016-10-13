cf7b095395b5434346fbdba94aa866d935dcf059
José Valderrama <jvalderrama2602@gmail.com>
2016-10-13 11:53:55 -0400

    Practica con Json en local funcionan en servidor PHP

--- Javascript-Practicas/app.js
+++ Javascript-Practicas/app.js
@@ -1,1 +1,12 @@
-app.js
+$.getJSON("productos.json",function(producto){
+
+
+var totalProductos=producto.length;
+
+for (var i=0;i<totalProductos;i++){
+
+console.log(producto[i].nombre);
+
+}
+
+})
